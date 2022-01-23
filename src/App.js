0ed27f98240bc9b8ce3aa16a2571ff8d4614512c
hejin-jeong <pearlofthesea25@gmail.com>
2022-01-22 21:39:24 -0500

    project start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import Home from "./pages/Home"
+import Card from "./pages/Card"
+import Profile from "./pages/Profile"
+import Map from "./pages/Map"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/card' element={<Card />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/map' element={<Map />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <a href="/card">Card</a>
+            <a href="/profile">Profile</a>
+            <a href="/map">Map</a>
+        </div>
+    )
+}
+
+export default Home;
