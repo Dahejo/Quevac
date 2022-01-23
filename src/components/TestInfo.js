fb2c098d139a13074a885a149603780df728c0ad
unknown <park49j@mtholyoke.edu>
2022-01-23 13:48:17 -0500

    profile page

---
+++ src/components/TestInfo.js
@@ -0,0 +1,38 @@
+export const TestInfo = [
+    {
+        id : 1,
+        result: true,
+        place: "Ma Clinique Lebourneuf",
+        date: new Date(2022,0,22).toDateString()
+    },
+    {
+        id : 2,
+        result: false,
+        place: "Ma Clinique Lebourneuf",
+        date: new Date(2022,0,4).toDateString()
+    },
+    {
+        id : 3,
+        result: true,
+        place: "Ma Clinique Lebourneuf",
+        date: new Date(2022,0,2).toDateString()
+    },
+    {
+        id : 4,
+        result: true,
+        place: "Ma Clinique Lebourneuf",
+        date: new Date(2021,11,6).toDateString()
+    },
+    {
+        id : 5,
+        result: true,
+        place: "Ma Clinique Lebourneuf",
+        date: new Date(2021,10,6).toDateString()
+    },
+    {
+        id : 6,
+        result: true,
+        place: "Ma Clinique Lebourneuf",
+        date: new Date(2021,9,15).toDateString()
+    },
+];
