d911f6a2a9f512e54197487bb9d5979a92ebccd0
hejin-jeong <pearlofthesea25@gmail.com>
2022-01-23 13:42:38 -0500

    create home and map page

---
+++ src/components/ListItems.js
@@ -0,0 +1,42 @@
+export const ListItems = [
+    {
+        id: 1,
+        name: "Centre de Vaccination COVID-Vaccination a l'Auto-Parc Colbert",
+        people: 12,
+        distance: 0.2,
+        hours: "09:00 - 14:00",
+        address: "2400 Av. Dalton, Québec",
+        tel: "000-000-0000",
+        details: "Appointment not required for Covid Test"
+    },
+    {
+        id: 2,
+        name: "Centre de Dépistage COVID-19-Cdd Lévis",
+        people: 7,
+        distance: 0.4,
+        hours: "09:00 - 14:00",
+        address: "50 Rte du Président-Kennedy #180, Lévis, QC G6V 6W8, Canada",
+        tel: "000-000-0000",
+        details: "not reuireds"
+    },
+    {
+        id: 3,
+        name: "Centre de Dépistage COVID-19-Cdd Port de Québec",
+        people: 12,
+        distance: 0.5,
+        hours: "09:00 - 14:00",
+        address: "84 Rue Dalhousie, Québec",
+        tel: "000-000-0000",
+        details: "not reuireds"
+    },
+    {
+        id: 4,
+        name: "fourth",
+        people: 12,
+        distance: 0.2,
+        hours: "09:00 - 14:00",
+        address: "saldfj",
+        tel: "000-000-0000",
+        details: "not reuireds"
+    }
+]
--- src/pages/Card.js
+++ src/pages/Card.js
@@ -1,9 +1,13 @@
 import React from "react";
+import './Card.css';
 
 function Card() {
     return (
-        <div>
-            <h1>Card</h1>
+        <div className="card-bg-image">
+            <h1 className="card-page-title">Vaccine Card</h1>
+            <div className="vaccine-card">
+                <h3 className="card-title">COVID-19 Immunization Record</h3>
+            </div>
         </div>
     )
 }
